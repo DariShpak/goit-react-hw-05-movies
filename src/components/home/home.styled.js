62cbac96aaa12b73df80558ded495d3e57fc8fb9
Daria Shpak <darya.shpak@gmail.com>
2023-08-27 01:25:13 +0300

    Create home.styled.js

---
+++ src/components/home/home.styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+  padding-top: 40px;
+  padding-bottom: 40px;
+
+  margin-top: 40px;
+  margin-bottom: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 980px;
+  height: 100%;
+  background-color: #99eeee;
+  background-color: rgba(153, 238, 238, 0.2);
+  border-radius: 20px;
+`
+
+export const Ul = styled.ul`
+padding:0px;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 984px;
+  justify-content: center;
+  gap: 32px;
+  margin: 0;
+`
