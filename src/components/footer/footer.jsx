1fe5cb30531a656d4fab734819ebe4da69e1c613
Daria Shpak <darya.shpak@gmail.com>
2023-08-27 01:24:46 +0300

    Create footer.jsx

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import {ReactComponent as TMDBIcon} from "icons/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+import {FooterWrap, Span, Text} from "./footer.styled"
+
+const Footer = () => {
+  return <FooterWrap>
+      <Span>
+        <TMDBIcon width="60" height="42" />
+        <Text>
+          This product uses the TMDB API but is not endorsed or certified by
+          TMDB
+        </Text>
+        <Text>Built with ❤️ by Dari Shpak</Text>
+      </Span>
+    </FooterWrap>
+}
+
+export default Footer
