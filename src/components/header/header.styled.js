c33acc367a4cddc0ed67ca643bd5ccc4096c01ab
Daria Shpak <darya.shpak@gmail.com>
2023-08-27 01:25:03 +0300

    Update header.styled.js

--- src/components/header/header.styled.js
+++ src/components/header/header.styled.js
@@ -1,12 +1,15 @@
 import styled from "styled-components"
 import {NavLink} from "react-router-dom"
 
-export const Container = styled.div`padding: 20px;`
+export const Container = styled.div`
+  padding: 20px;
+  border-bottom: 1px solid #99dddd;
+`
 
 export const Header = styled.header`
-  height: 60px;
+  height: 40px;
   display: flex;
-  gap: 100px;
+  justify-content: center;
   align-items: center;
 `
 
@@ -21,11 +24,22 @@ export const Ul = styled.ul`
   padding: 0;
 `
 
-export const Li = styled.li``
+export const Li = styled.li`
+  width: 80px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #99eeee;
+  background-color: rgba(153, 238, 238, 0.2);
+  border-radius: 20px;
+`
 
 export const NaviLink = styled(NavLink)`
 text-decoration: none;
 color: #ebecf0;
 font-size: 16px;
+font-weight: 500;
+text-transform: uppercase;
 
 `
