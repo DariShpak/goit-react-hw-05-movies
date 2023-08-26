91b2cf14aa024f0436edccccd052beb1165ebf9a
Daria Shpak <darya.shpak@gmail.com>
2023-08-27 01:25:17 +0300

    Create movieCard.styled.js

---
+++ src/components/movieCardHome/movieCard.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const Li = styled.li`list-style: none;`
+
+export const CardWrapper = styled.div`display: flex;
+         flex-direction: column;
+         gap: 14px;
+         width: 197px;
+         height: 327px;
+`
+
+
+export const Img = styled.img`
+  display: block;
+  border-radius: 20px;
+`
+export const H2 = styled.h2`color: #ebecf0;
+         font-size: 14px;
+         text-align: center;
+         margin: 0;
+      `
