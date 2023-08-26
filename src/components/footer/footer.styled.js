12f942a1271b539871f9b17dbdecd6f09e8cf31e
Daria Shpak <darya.shpak@gmail.com>
2023-08-27 01:24:51 +0300

    Create footer.styled.js

---
+++ src/components/footer/footer.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const FooterWrap = styled.div`
+padding-left:20px;
+padding-right: 20px;
+  background: rgba(153, 170, 170, 0.2);
+  border-top: 1px solid #99dddd;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`
+
+export const Span = styled.span`text-align: center;`
+
+export const Text = styled.p`
+  color: #ebecf0;
+  font-weight: 400;
+  font-size: 12px;
+`
