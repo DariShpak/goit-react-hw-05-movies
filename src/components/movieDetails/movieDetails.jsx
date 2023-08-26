a5c8e51de9b1e947999820284de77f2442871df7
Daria Shpak <darya.shpak@gmail.com>
2023-08-27 01:25:24 +0300

    Create movieDetails.jsx

---
+++ src/components/movieDetails/movieDetails.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MovieDetails = () => {
+  return <div>
+      <div>
+        <h1>Movie Title</h1>
+        <img src="" alt="movie title" />
+        <p>Genre</p>
+        <p>Genre</p>
+        <p>Film Description</p>
+        <p>Rating</p>
+    </div>
+    <div>Cast</div>
+    </div>
+}
+
+export default MovieDetails
