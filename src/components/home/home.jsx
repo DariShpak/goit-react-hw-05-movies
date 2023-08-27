5963b7543b7f9882ee413bc0457884617fb9cf22
Daria Shpak <darya.shpak@gmail.com>
2023-08-28 00:24:13 +0300

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,7 +2,7 @@ import {useState, useEffect} from "react"
 import {getTrendingMovies} from "fetches/FetchTrendings"
 
 import MovieCard from "components/movieCardHome/movieCardHome"
-import {Ul, Wrapper} from "./home.styled"
+import {Ul, Wrapper, H1} from "./home.styled"
 
 const Home = () => {
   const [trendings, setTrendings] = useState([])
@@ -23,6 +23,8 @@ const Home = () => {
 
   return (
     <Wrapper>
+      <H1>Trending Now</H1>
+
       <Ul>
         {trendings &&
           trendings.map(({id, title, poster_path}) =>
