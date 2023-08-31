445c560bd00c29400a5aa3cd205418032b9e4e11
Daria Shpak <darya.shpak@gmail.com>
2023-08-31 03:18:50 +0300

    Update footer.styled.js

--- src/components/footer/footer.styled.js
+++ src/components/footer/footer.styled.js
@@ -15,5 +15,6 @@ padding-right: 20px;
 export const Span = styled.span`text-align: center;`
 
 export const Text = styled.p`color: #ffffff;
+         text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
          font-weight: 400;
          font-size: 12px;`
