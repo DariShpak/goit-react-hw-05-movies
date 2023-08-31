942616aff9615fa3f57ea7cbd640ca3727c15f11
Daria Shpak <darya.shpak@gmail.com>
2023-08-31 03:16:39 +0300

    Update FetchTrendings.js

--- src/fetches/FetchTrendings.js
+++ src/fetches/FetchTrendings.js
@@ -1,9 +1,10 @@
 import axios from "axios"
+import {API_KEY} from "utils/API_KEY"
 
-const API_KEY = "c44bc15ca1931b55e279cdae9c283603"
 const timeWindow = "day"
 
 export const getTrendingMovies = async (page = 1) => {
+
   const response = await axios.get(
     `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${API_KEY}`
   )
