39c7e2e87a4a7a1c3bfd3d2550c8f15f11423b23
Daria Shpak <darya.shpak@gmail.com>
2023-08-31 03:17:29 +0300

    Create movieListItem.jsx

---
+++ src/components/movieListItem/movieListItem.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+import PropTypes from "prop-types"
+import { CardWrapper, Li, H2, Img} from "./movieListItem.styled"
+
+
+
+const MovieCard = ({title, poster_path}) => {
+  return <Li>
+      <CardWrapper>
+        <Img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="197" height="287"/>
+        <H2>
+          {title}
+        </H2>
+      </CardWrapper>
+    </Li>
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired
+}
+export default MovieCard
