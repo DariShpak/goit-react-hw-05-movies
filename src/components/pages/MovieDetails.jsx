67325a5a16b6fe163b8bf6a81e4856c2f4c69bf3
Daria Shpak <darya.shpak@gmail.com>
2023-08-31 03:17:12 +0300

    Update MovieDetails.jsx

--- src/components/pages/MovieDetails.jsx
+++ src/components/pages/MovieDetails.jsx
@@ -1,11 +1,60 @@
-import React from "react"
-
+import {useParams, Link} from "react-router-dom"
+import {useState, useEffect} from "react"
+import getMovieDetails from "fetches/FetchMovieDetails"
+import MovieDetailsItem from "components/movieDetails/movieDetailsItem"
 
 const MovieDetails = () => {
+  const {movieId} = useParams()
+  const [movieDetails, setMovieDetails] = useState({})
+  const [genres, setGenres] = useState([])
+  // const [isLoading, setIsLoading] = useState(false)
+
+  const allGenres = genres.map(genre => genre.name)
+
+  useEffect(
+    () => {
+      const fetchMovieDetails = async () => {
+        try {
+          // setIsLoading(true)
+          const response = await getMovieDetails(movieId)
+          const movieData = response.data
+
+          const releaseDate = movieData.release_date.slice(0, 4)
+        const voteAverage = movieData.vote_average.toFixed(2)
+
+          setMovieDetails({
+            ...movieData,
+            release_date: releaseDate,
+            vote_average: voteAverage
+          })
+          setGenres(movieData.genres)
+        } catch (error) {
+          console.log(error)
+        } finally {
+          // setIsLoading(false)
+        }
+      }
+      fetchMovieDetails()
+    },
+    [movieId]
+  )
+
   return (
     <div>
-movie
+      <Link to="/">Back</Link>
+      <MovieDetailsItem
+        title={movieDetails.title}
+        genres={allGenres}
+        overview={movieDetails.overview}
+        poster={movieDetails.poster_path}
+        release={movieDetails.release_date}
+        tagline={movieDetails.tagline}
+        vote={movieDetails.vote_average}
+      />
+      <Link to="/cast">Cast</Link>
+      <Link to="/reviews">Reviews</Link>
     </div>
   )
 }
+
 export default MovieDetails
