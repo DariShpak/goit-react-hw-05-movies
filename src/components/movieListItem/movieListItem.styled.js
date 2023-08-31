0f7ef2b3f72874bc21e91cda9d258a46767b6412
Daria Shpak <darya.shpak@gmail.com>
2023-08-31 03:17:26 +0300

    Create movieListItem.styled.js

---
+++ src/components/movieListItem/movieListItem.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Li = styled.li`list-style: none;`
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  width: 197px;
+  height: 327px;
+`
+
+export const Img = styled.img`
+  display: block;
+  border-radius: 20px;
+`
+export const H2 = styled.h2`color: #ffffff;
+         font-size: 18px;
+         font-weight: 500;
+         text-align: center;
+         margin: 0;
+         text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);`
