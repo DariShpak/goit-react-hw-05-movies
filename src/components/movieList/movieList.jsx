d9d78aa50f8ea2aa74a9e6dba73d9e00353dc34e
Daria Shpak <darya.shpak@gmail.com>
2023-08-31 03:17:35 +0300

    Create movieList.jsx

---
+++ src/components/movieList/movieList.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import {Li, CardWrapper, Img, H2 } from "./movieList.styled"
+
+const MovieGallery = ({title, poster}) => {
+  const defaultPoster = "https://www.movienewz.com/img/films/poster-holder.jpg"
+
+  const handleImageError = event => {
+    event.target.src = defaultPoster
+  }
+
+  return (
+    <Li>
+      <CardWrapper>
+        <Img
+          src={`https://image.tmdb.org/t/p/w500${poster}`}
+          alt={title}
+          width="197"
+          height="287"
+          onError={handleImageError}
+        />
+
+        <H2>
+          {title}
+        </H2>
+      </CardWrapper>
+    </Li>
+  )
+}
+
+export default MovieGallery
