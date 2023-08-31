a65cdca0fdbfce456d452e543d7ac9c86ed7c7ac
Daria Shpak <darya.shpak@gmail.com>
2023-08-31 03:17:22 +0300

    Update movies.jsx

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -1,19 +1,26 @@
 // import axios from "axios"
 import {useState, useEffect} from "react"
-import {Form, Input, Section, MovieList, Wrapper} from "./movies.styled"
+import {Form, Input, Section, MovieList, Wrapper, Button} from "./movies.styled"
 import getMovieByName from "fetches/FetchMovieByName"
-import MovieGallery from "components/movieGallery/movieGallery"
+import MovieGallery from "components/movieList/movieList"
 
 const MoviesSearch = () => {
   const [name, setName] = useState("")
   const [page, setPage] = useState(1)
   const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState(false)
 
   const onHandleSearch = event => {
     event.preventDefault()
-    const inputValue = event.target[0].value.toLowerCase()
+    const inputValue = event.target.value
+
     setName(inputValue)
     setPage(1)
+  }
+
+  const handleSearchSubmit = event => {
+    event.preventDefault()
+    setSearch(true)
   }
 
   useEffect(
@@ -21,31 +28,43 @@ const MoviesSearch = () => {
       const fetchMovies = async () => {
         try {
           const response = await getMovieByName(name, page)
-          const newMovies = response.data.results
-          console.log(newMovies)
+          const newMovies = response.results
           setMovies([...newMovies])
+          setSearch(false)
         } catch (error) {
           console.log(error)
+          setSearch(false)
         }
       }
-
-      fetchMovies()
+      if (search) {
+        fetchMovies()
+      }
     },
-    [name, page]
+    [name, page, search]
   )
 
-  return <Section>
-      <Form onSubmit={onHandleSearch}>
-        <Input type="text" name="name" autoComplete="off" placeholder="Search a movie" onChange={onHandleSearch} />
+  return (
+    <Section>
+      <Form onSubmit={handleSearchSubmit}>
+        <Input
+          type="text"
+          name="name"
+          autoComplete="off"
+          placeholder="Search a movie"
+          onChange={onHandleSearch}
+        />
+        <Button type="submit">GO</Button>
       </Form>
       <Wrapper>
         <MovieList>
-          {movies && movies.map(({id, title, poster_path, }) =>
-            <MovieGallery key={id} title={title} poster={poster_path}  />
+          {movies &&
+            movies.map(({id, title, poster_path}) =>
+              <MovieGallery key={id} title={title} poster={poster_path} />
             )}
         </MovieList>
       </Wrapper>
     </Section>
+  )
 }
 
 export default MoviesSearch
