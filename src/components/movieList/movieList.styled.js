2f32c76c66edb150d76e03cfb9931a77e424ead5
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:40:15 +0300

    Update movieList.styled.js

--- src/components/movieList/movieList.styled.js
+++ src/components/movieList/movieList.styled.js
@@ -14,8 +14,10 @@ export const Img = styled.img`
   display: block;
   border-radius: 20px;
 `
-export const H2 = styled.h2`color: #ffffff;
-         text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
-         font-size: 18px;
-         text-align: center;
-         margin: 0;`
+export const H2 = styled.h2`
+  color: #ffffff;
+  text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
+  font-size: 18px;
+  text-align: center;
+  margin: 0;
+`
