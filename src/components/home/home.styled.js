2b1a229b6d9e397f654b49672a7691cccd3361eb
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:38:18 +0300

    Updates home page styles

--- src/components/home/home.styled.js
+++ src/components/home/home.styled.js
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
   margin-bottom: 40px;
   margin-left: auto;
   margin-right: auto;
-  max-width: 980px;
+  max-width: 1240px;
   height: 100%;
   background-color: #99eeee;
   background-color: rgba(153, 238, 238, 0.2);
@@ -16,21 +16,26 @@ export const Wrapper = styled.div`
 `
 
 export const Ul = styled.ul`
-padding:0px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0px;
   list-style: none;
   display: flex;
   flex-wrap: wrap;
-  max-width: 984px;
+
   justify-content: center;
-  gap: 32px;
+  gap: 34px;
   margin: 0;
 `
-export const H1 = styled.h1`color: #ffffff;
-         font-size: 20px;
-         text-align: center;
-         margin-top: 0;
-         margin-bottom: 20px;
-         font-weight: 500;
-         text-transform: uppercase;
+export const H1 = styled.h1`
+  color: #ffffff;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 0;
+  margin-bottom: 40px;
+  font-weight: 500;
+  text-transform: uppercase;
 
-         text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);`
+  text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
+`
+export const Li = styled.li`list-style: none;`
