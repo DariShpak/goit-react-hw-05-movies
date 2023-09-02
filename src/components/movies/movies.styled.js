798cd48c25f6365d33d4552d5660cf987b8ec371
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:40:27 +0300

    Update movies.styled.js

--- src/components/movies/movies.styled.js
+++ src/components/movies/movies.styled.js
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom"
 import styled from "styled-components"
+
 
 export const Section = styled.section`
   display: flex;
@@ -7,14 +9,15 @@ export const Section = styled.section`
   gap: 40px;
   margin-top: 40px;
   margin-bottom: 40px;
-  min-height: 530px;
+  height: 100%;
 `
 
 export const Form = styled.form`
-display: flex;
-gap: 18px;
-justify-content: center;
-align-items: center;`
+  display: flex;
+  gap: 18px;
+  justify-content: center;
+  align-items: center;
+`
 
 export const Input = styled.input`
   width: 630px;
@@ -39,44 +42,55 @@ export const Input = styled.input`
     background-color: #99dddd;
     background-color: rgba(153, 221, 221, 0.6);
   }
-
-
 `
 
 export const Wrapper = styled.div`
   padding-top: 40px;
   padding-bottom: 40px;
 
-
   margin-bottom: 40px;
   margin-left: auto;
   margin-right: auto;
-  max-width: 980px;
+  max-width: 1240px;
   height: 100%;
   background-color: #99eeee;
   background-color: rgba(153, 238, 238, 0.2);
   border-radius: 20px;
 `
 
-export const MovieList = styled.ul`
+export const Ul = styled.ul`
   padding: 0px;
   list-style: none;
   display: flex;
   flex-wrap: wrap;
-  max-width: 984px;
+
   justify-content: center;
   gap: 32px;
   margin: 0;
 `
-export const Button = styled.button`outline: transparent;
-cursor: pointer;
-         border: none;
-         border-radius: 50px;
-         width: 30px;
-         height: 29px;
-         outline: transparent;
-font-size:12px;
-         color: #ebecf0;
-         background-color: #99eeee;
-         background-color: rgba(153, 238, 238, 0.4);`
-  
+export const Button = styled.button`
+  outline: transparent;
+  cursor: pointer;
+  border: none;
+  border-radius: 50px;
+  width: 30px;
+  height: 29px;
+  outline: transparent;
+  font-size: 12px;
+  color: #ebecf0;
+  background-color: #99eeee;
+  background-color: rgba(153, 238, 238, 0.4);
+`
+
+export const Error = styled.div`
+  text-align: center;
+  margin-top: 30px;
+`
+
+export const ErrorText = styled.h1`
+  font-size: 36px;
+  color: #5b6988;
+`
+
+
+
