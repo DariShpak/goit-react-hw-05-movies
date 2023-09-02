69d791b98d6abbaad8fe7c8df4c4e8656cf8ce75
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:42:13 +0300

    Create scrollUp.styled.js

---
+++ src/components/scrollUpButton/scrollUp.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const BtnUp = styled.button`
+top: 68%;
+left: 98%;  
+position: sticky;
+  outline: transparent;
+  cursor: pointer;
+  border: none;
+  border-radius: 50px;
+  width: 50px;
+  height: 50px;
+  outline: transparent;
+  padding: 2px;
+  color: #ebecf0;
+  background-color: #99eeee;
+  background-color: rgba(153, 238, 238, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
