8ed326f1ad82f5c72c7511ce90bd9d04c07440ef
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:39:36 +0300

    Adds styles to loader icon component

---
+++ src/components/loader/loader.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const LoaderOverlay = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
