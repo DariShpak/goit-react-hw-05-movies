ebfca6efec1e02db01c40ba485827f1c33668c77
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:42:26 +0300

    Update FetchMovieByName.js

--- src/fetches/FetchMovieByName.js
+++ src/fetches/FetchMovieByName.js
@@ -4,7 +4,7 @@ import {API_KEY} from "utils/API_KEY"
 const getMovieByName = async (name, page = 1) => {
   try {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?query=${name}&page=${page}&api_key=${API_KEY}`
+      `https://api.themoviedb.org/3/search/movie?&query=${name}&page=${page}&api_key=${API_KEY}`
     )
     return response.data
   } catch (error) {
