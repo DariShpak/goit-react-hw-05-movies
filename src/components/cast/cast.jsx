a97ca2b012acd71f41bd93d8baba37e53d6d9cb0
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:35:49 +0300

    Creates Cast component, adds fetch and markup

---
+++ src/components/cast/cast.jsx
@@ -0,0 +1,47 @@
+import {useState, useEffect} from "react"
+import getCast from "fetches/fetchCast"
+import {useParams} from "react-router-dom"
+import CastItem from "./castItem"
+import { LoaderIcon } from "components/loader/loader"
+import { CastWrapper, CastList} from "./cast.styled"
+
+const Cast = () => {
+  const {movieId} = useParams()
+  const [cast, setCast] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  // const [error, setError] = useState(null)
+
+  useEffect(
+    () => {
+      const fetchAllCast = async () => {
+        try {
+          setIsLoading(true)
+          const castData = await getCast(movieId)
+          const newCast = castData.data.cast
+
+          setCast([...newCast])
+        } catch (error) {
+          console.log(error)
+        } finally {
+          setIsLoading(false)
+        }
+      }
+      fetchAllCast()
+    },
+    [movieId]
+  )
+
+  return (
+    <CastWrapper>
+      {isLoading && <LoaderIcon />}
+      <CastList>
+        {cast &&
+          cast.map(({id, name, profile_path}) =>
+            <CastItem key={id} name={name} photo={profile_path} />
+          )}
+      </CastList>
+    </CastWrapper>
+  )
+}
+
+export default Cast
