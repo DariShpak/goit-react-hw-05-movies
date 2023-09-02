dae7165a7aac6aaa03d29f83fffd006cca20c09d
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:40:19 +0300

    Update movieListItem.jsx

--- src/components/movieListItem/movieListItem.jsx
+++ src/components/movieListItem/movieListItem.jsx
@@ -1,22 +1,28 @@
 import React from "react"
 
 import PropTypes from "prop-types"
-import { CardWrapper, Li, H2, Img} from "./movieListItem.styled"
+import {CardWrapper, H2, Img, Li} from "./movieListItem.styled"
 
+const MovieListItem = ({title, poster}) => {
+  return (
+    <Li>    <CardWrapper>
+      <Img
+        src={`https://image.tmdb.org/t/p/w500${poster}`}
+        alt={title}
+        width="207"
+        height="307"
+        loading="lazy"
+      />
+      <H2>
+        {title}
+      </H2>
+    </CardWrapper></Li>
 
-
-const MovieCard = ({title, poster_path}) => {
-  return <Li>
-      <CardWrapper>
-        <Img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="197" height="287"/>
-        <H2>
-          {title}
-        </H2>
-      </CardWrapper>
-    </Li>
+  )
 }
 
-MovieCard.propTypes = {
-  title: PropTypes.string.isRequired
+MovieListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
-export default MovieCard
+export default MovieListItem
