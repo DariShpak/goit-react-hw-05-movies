d0162375b5b7f5144be7d5bfe96de7f99458bd67
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:42:10 +0300

    Create reviews.styled.js

---
+++ src/components/reviews/reviews.styled.js
@@ -0,0 +1,36 @@
+import styled from "styled-components"
+
+export const ReviewsSection = styled.section`max-width: 980px;`
+
+export const ReviewsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`
+
+export const ReviewsListItem = styled.li`
+  background-color: #99eeee;
+  background-color: rgba(153, 238, 238, 0.2);
+  border-radius: 20px;
+  padding: 40px;
+  color: #ffffff;
+  text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
+  font-size: 14px;
+`
+
+export const Author = styled.h4`margin-top: 0;`
+
+export const Content = styled.p`margin: 0;`
+
+export const ErrorWrap = styled.div`
+max-width=980px;`
+
+export const ErrorText = styled.h1`
+  color: #ffffff;
+  text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
+  font-size: 14px;
+  background-color: #99eeee;
+  background-color: rgba(153, 238, 238, 0.2);
+  border-radius: 20px;
+  padding: 40px;
+`
