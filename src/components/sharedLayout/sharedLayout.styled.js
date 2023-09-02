3a17e548777857a79efde9e5f4f62c2e819af04f
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:42:21 +0300

    Create sharedLayout.styled.js

---
+++ src/components/sharedLayout/sharedLayout.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Main = styled.main`
+  padding-left: 20px;
+  min-height: 77vh;
+  padding-right: 20px;
+  display: flex;
+  justify-content: center;
+`
