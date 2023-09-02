f1b795125cf376033e395bc6ab65f35be7205d9a
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:42:29 +0300

    Update FetchMovieDetails.js

--- src/fetches/FetchMovieDetails.js
+++ src/fetches/FetchMovieDetails.js
@@ -2,10 +2,15 @@ import axios from "axios"
 import {API_KEY} from "utils/API_KEY"
 
 const getMovieDetails = async movieId => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&api_key=${API_KEY}`
-  )
- return response
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&api_key=${API_KEY}`
+    )
+    return response
+  } catch (error) {
+    console.error("Error fetching movies:", error)
+    throw error
+  }
 }
 
 export default getMovieDetails
