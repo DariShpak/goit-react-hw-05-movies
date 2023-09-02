df99a897e6600ef84692a4e2fb34b659d7145809
Daria Shpak <darya.shpak@gmail.com>
2023-09-02 03:42:36 +0300

    Update FetchTrendings.js

--- src/fetches/FetchTrendings.js
+++ src/fetches/FetchTrendings.js
@@ -3,11 +3,17 @@ import {API_KEY} from "utils/API_KEY"
 
 const timeWindow = "day"
 
-export const getTrendingMovies = async (page = 1) => {
+const getTrendingMovies = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${API_KEY}`
+    )
 
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${API_KEY}`
-  )
-
-  return response
+    return response
+  } catch (error) {
+    console.error("Error fetching cast:", error)
+    throw error
+  }
 }
+
+export default getTrendingMovies
