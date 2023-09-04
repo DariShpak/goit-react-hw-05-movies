43e1a57d9b1b67feb0494145ab5b01f8f1d2b047
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:57:21 +0300

    Update moviesComponent.jsx

--- src/components/movies/moviesComponent.jsx
+++ src/components/movies/moviesComponent.jsx
@@ -1,54 +1,108 @@
-import React from "react"
+import { useState, useEffect, useMemo } from "react"
 import MovieList from "components/movieList/movieList"
 import { Link } from "react-router-dom"
-import {
-  Form,
-  Input,
-  Section,
-  Ul,
-  Wrapper,
-  Button,
-  Error,
-  ErrorText,
+import MovieSearchForm from "components/searchMovieForm.jsx/searchMovieForm"
+import getMovieByName from "fetches/fetchMovieByName"
+import InfiniteScroll from "react-infinite-scroll-component"
+import { LoaderIcon } from "components/loader/loader"
+import {Section,Ul,Wrapper} from "components/movies/movies.styled"
+import ErrorSection from "components/error/error"
 
-} from "components/movies/movies.styled"
-import PropTypes from "prop-types"
+const MoviesComponent = () => {
+  const [name, setName] = useState("")
+  const [page, setPage] = useState(1)
+  const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState(false)
+  const [hasMore, setHasMore] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-const MoviesComponent = ({movies, error, onSubmit, onChange}) => {
-  return <Section>
-      <Form onSubmit={onSubmit}>
-        <Input type="text" name="name" autoComplete="off" placeholder="Search a movie" onChange={onChange} />
-        <Button type="submit">GO</Button>
-      </Form>
+  const onHandleSearch = event => {
+    event.preventDefault()
+    const inputValue = event.target.value
 
-      <Wrapper>
-        {error && <Error>
-            <ErrorText>
-              "{error}"
-            </ErrorText>
-          </Error>}
+    setName(inputValue)
+    setPage(1)
+    setMovies([])
+    setError(null)
+  }
 
-        <Ul>
-          {movies && movies.map(({id, title, poster_path}) =>
+  const handleSearchSubmit = event => {
+    event.preventDefault()
+    setSearch(true)
+  }
+
+  const fetchMovies = useMemo(
+    () =>
+      async function() {
+        try {
+          setIsLoading(true)
+          const response = await getMovieByName(name, page)
+          const newMovies = response.results
+
+          setMovies(prevMovies => [...prevMovies, ...newMovies])
+          setPage(prevPage => prevPage + 1)
+          setSearch(false)
+          setError(null)
+
+          if (page === 100) {
+            setHasMore(false)
+          }
+        } catch (error) {
+          setError(error)
+          setSearch(false)
+        } finally {
+          setIsLoading(false)
+        }
+      },
+    [name, page]
+  )
+
+  const loadMoreMovies = () => {
+    if (!search) {
+      fetchMovies()
+    }
+  }
+
+  useEffect(
+    () => {
+      if (search) {
+        fetchMovies()
+      }
+    },
+    [fetchMovies, search]
+  )
+
+  return (
+    <>
+      <MovieSearchForm onSubmit={handleSearchSubmit} onChange={onHandleSearch} />
+      {error && <ErrorSection error={ error } />}
+      <Section>
+         <InfiniteScroll
+          dataLength={movies.length}
+          next={loadMoreMovies}
+          style={{overflow: "hidden"}}
+          hasMore={hasMore}
+          loader={isLoading && <LoaderIcon />}
+        >
+   <Wrapper>
+          <Ul>
+          {movies &&
+            movies.map(({id, title, poster_path}) =>
               <Link key={id} to={`/movies/${id}`}>
                 <MovieList key={id} title={title} poster={poster_path} />
               </Link>
             )}
-        </Ul>
-      </Wrapper>
-    </Section>
+          </Ul>
+        </Wrapper>
+      </InfiniteScroll>
+      
+       
+      </Section>
+    </>
+    
+  )
 }
 
-MoviesComponent.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      poster_path: PropTypes.string.isRequired
-    })
-  ),
-  error: PropTypes.string,
-  onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-}
+
 export default MoviesComponent
