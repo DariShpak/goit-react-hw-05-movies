7a0e27c5c6beb6ca222f943149d076d2c397c4e0
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:58:04 +0300

    Create moviesPage.jsx

---
+++ src/pages/moviesPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import MoviesComponent from "components/movies/moviesComponent"
+
+const Movies = () => {
+ 
+  return (
+    <>
+     <MoviesComponent />
+     </>
+
+  )
+}
+
+export default Movies
