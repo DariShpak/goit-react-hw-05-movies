a5ccaf5626799297a28c481541b98de3af4af6c3
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:55:41 +0300

    Updates header markup

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,20 @@
 import React from "react"
-import {Container, Header, Nav, Ul, Li, NaviLink} from "./header.styled"
+import {Header, Nav, Ul, Li, NaviLink} from "./header.styled"
 
 const HeaderBar = () => {
   return (
-    <Container>
-      <Header>
-        <Nav>
-          <Ul>
-            <Li>
-              <NaviLink to="/">Home</NaviLink>
-            </Li>
-            <Li>
-              <NaviLink to="/movies">Movies</NaviLink>
-            </Li>
-          </Ul>
-        </Nav>
-      </Header>
-    </Container>
+    <Header>
+      <Nav>
+        <Ul>
+          <Li>
+            <NaviLink to="/">Home</NaviLink>
+          </Li>
+          <Li>
+            <NaviLink to="/movies">Movies</NaviLink>
+          </Li>
+        </Ul>
+      </Nav>
+    </Header>
   )
 }
 
