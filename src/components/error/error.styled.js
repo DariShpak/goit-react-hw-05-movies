48a57b0e4c6b89e60f210d9f88ff929711446fad
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:54:54 +0300

    Adds styles to error section

---
+++ src/components/error/error.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const ErrorWrap = styled.div`max-width: 1000px;`
+
+export const ErrorText = styled.h1`
+  font-size: 14px;
+  background-color: var(--section-background);
+  border-radius: var(--border-radius);
+  padding: 20px;
+`
