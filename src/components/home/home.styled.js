2a110516fcbebcb2290b8b77a963a09003c52dcb
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:56:14 +0300

    Updates home page styles

--- src/components/home/home.styled.js
+++ src/components/home/home.styled.js
@@ -3,39 +3,32 @@ import styled from "styled-components"
 export const Wrapper = styled.div`
   padding-top: 40px;
   padding-bottom: 40px;
-
+  padding-left: 20px;
+  padding-right: 20px;
   margin-top: 40px;
   margin-bottom: 40px;
   margin-left: auto;
   margin-right: auto;
-  max-width: 1240px;
+  max-width: 1200px;
   height: 100%;
-  background-color: #99eeee;
-  background-color: rgba(153, 238, 238, 0.2);
-  border-radius: 20px;
+  background-color: var(--section-background);
+  border-radius: var(--border-radius);
 `
 
 export const Ul = styled.ul`
   margin-left: auto;
   margin-right: auto;
-  padding: 0px;
-  list-style: none;
   display: flex;
   flex-wrap: wrap;
 
   justify-content: center;
-  gap: 34px;
-  margin: 0;
+  gap: 24px;
 `
 export const H1 = styled.h1`
-  color: #ffffff;
   font-size: 20px;
   text-align: center;
-  margin-top: 0;
   margin-bottom: 40px;
   font-weight: 500;
   text-transform: uppercase;
-
-  text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
 `
-export const Li = styled.li`list-style: none;`
+export const Li = styled.li``
