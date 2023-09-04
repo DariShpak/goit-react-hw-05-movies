5e97ec70d760302bbd817ab0eeb92519a3b21022
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 05:13:40 +0300

    Update sharedLayout.jsx

--- src/components/sharedLayoutComponent/sharedLayout.jsx
+++ src/components/sharedLayoutComponent/sharedLayout.jsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router-dom"
 import HeaderBar from "components/header/header"
 import Footer from "components/footer/footer"
 import BtnScrollUp from "components/scrollUpButton/scrollUpButton"
-import {Main, Container} from "components/sharedLayout/sharedLayout.styled"
+import {Main, Container} from "./sharedLayout.styled"
 
 const SharedLayout = () => {
   return (
