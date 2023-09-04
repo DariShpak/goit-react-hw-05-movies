31e351f14ed38863fc1ef2936f572d9ec509299f
Daria Shpak <darya.shpak@gmail.com>
2023-09-05 02:41:19 +0300

    Update movieDetailsPage.jsx

--- src/pages/movieDetailsPage.jsx
+++ src/pages/movieDetailsPage.jsx
@@ -1,31 +1,46 @@
-import {Link, Outlet} from "react-router-dom"
+import { useRef } from "react"
+import { Link, Outlet, useLocation } from "react-router-dom"
+import {Suspense} from "react"
 import MovieDetailsItem from "components/movieDetailsItem/movieDetailsItem"
+import {LoaderIcon} from "components/loader/loader"
 import {
   Ul,
   Li,
-  StyledLink, Section,
+  StyledLink,
+  Section,
+  Button
 } from "components/movieDetailsItem/movieDetailsItem.styled"
 
 const MovieDetails = () => {
-  return <Section>
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? "/movies")
+  
+  return (
+    <Section>
+      <Button>
+        <Link to={backLinkLocationRef.current}>Go Back</Link>
+      </Button>
+
       <MovieDetailsItem />
 
       <Ul>
-          <Li>
-            <Link to="cast">
-              <StyledLink>Cast</StyledLink>
-            </Link>
-          </Li>
-          <Li>
-            <Link to="reviews">
-              <StyledLink>Reviews</StyledLink>
-            </Link>
-          </Li>
-        </Ul>
+        <Li>
+          <Link to="cast">
+            <StyledLink>Cast</StyledLink>
+          </Link>
+        </Li>
+        <Li>
+          <Link to="reviews">
+            <StyledLink>Reviews</StyledLink>
+          </Link>
+        </Li>
+      </Ul>
 
-
-      <Outlet />
+      <Suspense fallback={<LoaderIcon />}>
+        <Outlet />
+      </Suspense>
     </Section>
+  )
 }
 
 export default MovieDetails
