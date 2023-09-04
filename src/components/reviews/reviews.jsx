e7b8ab429f9c72de12a2d044ad247aa95909015d
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:57:36 +0300

    Update reviews.jsx

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -4,43 +4,61 @@ import ReviewItem from "./reviewItem"
 import getMovieReviews from "fetches/fetchReviews"
 import {LoaderIcon} from "components/loader/loader"
 import {ReviewsSection, ReviewsList} from "./reviews.styled"
-import Error from "./reviewError"
+import ErrorSection from "components/error/error"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 const Reviews = () => {
   const {movieId} = useParams()
   const [reviews, setReviews] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
+  const [hasMore, setHasMore] = useState(true)
+  const [page, setPage] = useState(1)
 
   useEffect(
     () => {
       const fetchAllReviews = async () => {
         try {
           setIsLoading(true)
-          const response = await getMovieReviews(movieId)
+          const response = await getMovieReviews(movieId, page)
           const reviewData = response.results
 
           if (reviewData.length === 0) {
             setError("Oooops, there are no reviews yet")
             setReviews([])
           } else {
-            setReviews([...reviewData])
+            setReviews(prevReviews => [...prevReviews, ...reviewData])
             setError(null)
           }
         } catch (error) {
+          setError(error)
+          
         } finally {
+          setHasMore(false)
           setIsLoading(false)
         }
       }
       fetchAllReviews()
     },
-    [movieId]
+    [movieId, page]
   )
+
+  const loadMoreReviews = () => {
+    setPage(prevPage => prevPage + 1)
+  }
 
   return (
     <ReviewsSection>
       {isLoading && <LoaderIcon />}
-      {error && <Error error={error} />}
+      {error && <ErrorSection error={error} />}
+
+      <InfiniteScroll
+        dataLength={reviews.length}
+        next={loadMoreReviews}
+        style={{overflow: "hidden"}}
+        hasMore={hasMore}
+        loader={isLoading && <LoaderIcon />}
+      />
 
       <ReviewsList>
         {reviews.length > 0
