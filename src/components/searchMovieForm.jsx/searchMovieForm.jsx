82ac33d79866e282257b5d408c952f3de82ebfac
Daria Shpak <darya.shpak@gmail.com>
2023-09-05 02:38:40 +0300

    Updates movieSearchForm logic

--- src/components/searchMovieForm.jsx/searchMovieForm.jsx
+++ src/components/searchMovieForm.jsx/searchMovieForm.jsx
@@ -1,15 +1,27 @@
 import React from "react"
+import { useSearchParams } from "react-router-dom"
 import {Form, Input, Button} from "./searchMovieForm.styled"
 
-const MovieSearchForm = ({onSubmit, onChange}) => {
+const MovieSearchForm = ({ onSubmit }) => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const query = searchParams.get('query') ?? "";
+
+  const updateQueryString = event => {
+    if (event.target.value === "") {
+     return setSearchParams({})
+    } 
+      setSearchParams({query: event.target.value})  
+  }
+
   return (
-    <Form onSubmit={onSubmit}>
+    <Form onSubmit={ onSubmit}>
       <Input
         type="text"
         name="name"
         autoComplete="off"
         placeholder="Search a movie"
-        onChange={onChange}
+        value={query }
+        onChange={updateQueryString}
       />
       <Button type="submit">GO</Button>
     </Form>
