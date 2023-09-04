48d217bda104d59e4e6998e13a275304ae5537ea
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 04:30:12 +0300

    Please

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Movies from "../pages/moviesPage"
 import MovieDetails from "../pages/movieDetailsPage"
 import Cast from "components/cast/cast"
 import Reviews from "components/reviews/reviews"
-import SharedLayout from "components/SharedLayout/SharedLayout"
+import SharedLayout from "./SharedLayout/SharedLayout"
 import Home from "../pages/homePage"
 import NotFound from "pages/notFoundPage"
 
