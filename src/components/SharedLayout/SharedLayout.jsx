f33f37e7b9243c6cb92cb074356e1f948484c8c6
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 04:21:07 +0300

    Trying to solve commit problem

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,20 @@
-
-import { Outlet } from "react-router-dom";
-import HeaderBar from "components/header/header";
+import {Outlet} from "react-router-dom"
+import HeaderBar from "components/header/header"
 import Footer from "components/footer/footer"
-import BtnScrollUp from "components/scrollUpButton/scrollUpButton";
-import { Main, Container } from "./sharedLayout.styled";
+import BtnScrollUp from "components/scrollUpButton/scrollUpButton"
+import {Main, Container} from "./sharedLayout.styled"
 
 const SharedLayout = () => {
-  return <Container>
-    <HeaderBar />
-    <Main>
-    <BtnScrollUp />
-     <Outlet />
-    </Main>
-    <Footer/>
-  </Container>
+  return (
+    <Container>
+      <HeaderBar />
+      <Main>
+        <BtnScrollUp />
+        <Outlet />
+      </Main>
+      <Footer />
+    </Container>
+  )
 }
 
 export default SharedLayout
