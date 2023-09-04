40d0f08325058d594ba203c144859a4280cfbe4d
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:55:13 +0300

    Updates footer markup

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import {ReactComponent as TMDBIcon} from "icons/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
-import {FooterWrap, Span, Text} from "./footer.styled"
+import {FooterSection, Span, Text} from "./footer.styled"
 
 const Footer = () => {
-  return <FooterWrap>
+  return <FooterSection>
       <Span>
         <TMDBIcon width="60" height="42" />
         <Text>
@@ -12,7 +12,7 @@ const Footer = () => {
         </Text>
         <Text>Built with ❤️ by Dari Shpak</Text>
       </Span>
-    </FooterWrap>
+    </FooterSection>
 }
 
 export default Footer
