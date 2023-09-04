c69246e8060d2b17318721b912613125b97b0e58
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:57:39 +0300

    Update reviews.styled.js

--- src/components/reviews/reviews.styled.js
+++ src/components/reviews/reviews.styled.js
@@ -1,36 +1,27 @@
 import styled from "styled-components"
 
-export const ReviewsSection = styled.section`max-width: 980px;`
+export const ReviewsSection = styled.section`margin-left: auto;
+         margin-right: auto;
+         margin-bottom: 24px;`
 
 export const ReviewsList = styled.ul`
+  max-width: 1000px;
   display: flex;
   flex-direction: column;
-  gap: 22px;
+  gap: 24px;
+
 `
 
 export const ReviewsListItem = styled.li`
-  background-color: #99eeee;
-  background-color: rgba(153, 238, 238, 0.2);
-  border-radius: 20px;
+  background-color: var(--section-background);
+  border-radius: var(--border-radius);
   padding: 40px;
-  color: #ffffff;
-  text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
   font-size: 14px;
 `
 
-export const Author = styled.h4`margin-top: 0;`
+export const Author = styled.h4`
+margin-bottom: 10px;`
 
-export const Content = styled.p`margin: 0;`
+export const Content = styled.p``
 
-export const ErrorWrap = styled.div`
-max-width=980px;`
 
-export const ErrorText = styled.h1`
-  color: #ffffff;
-  text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
-  font-size: 14px;
-  background-color: #99eeee;
-  background-color: rgba(153, 238, 238, 0.2);
-  border-radius: 20px;
-  padding: 40px;
-`
