455e68e492389962d17e8a1bb3b4534a70354597
Daria Shpak <darya.shpak@gmail.com>
2023-09-04 03:55:26 +0300

    Updates footer styles

--- src/components/footer/footer.styled.js
+++ src/components/footer/footer.styled.js
@@ -1,20 +1,22 @@
 import styled from "styled-components"
 
-export const FooterWrap = styled.div`
-padding-left:20px;
-padding-right: 20px;
-  background: rgba(153, 170, 170, 0.2);
+export const FooterSection = styled.footer`
   border-top: 1px solid #99dddd;
   height: auto;
-  display: flex;
+  padding-top: 10px;
+  padding-bottom: 10px;
   justify-content: center;
-  padding-top: 15px;
-  padding-bottom: 15px;
+  display: flex;
+  flex-direction: column;
 `
 
-export const Span = styled.span`text-align: center;`
+export const Span = styled.span`
+  margin-top: 10px;
+  text-align: center;
+`
 
-export const Text = styled.p`color: #ffffff;
-         text-shadow: 3px 4px 7px rgba(81, 67, 21, 0.8);
-         font-weight: 400;
-         font-size: 12px;`
+export const Text = styled.p`
+  margin-top: 10px;
+  font-weight: 400;
+  font-size: 12px;
+`
