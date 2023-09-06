9675b4dfee81a60843f5b8307d79f99d0310b757
Daria Shpak <darya.shpak@gmail.com>
2023-09-07 02:19:44 +0300

    Updates named export to default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-import { lazy } from "react"
+import {lazy} from "react"
 import {Routes, Route} from "react-router-dom"
 
-const SharedLayout = lazy(() => import("./sharedLayoutComponent/sharedLayout"));
-const Home = lazy(() => import("../pages/homePage"));
-const Movies = lazy(() => import("../pages/moviesPage"));
-const MovieDetails = lazy(() => import("../pages/movieDetailsPage")) 
+const SharedLayout = lazy(() => import("./sharedLayoutComponent/sharedLayout"))
+const Home = lazy(() => import("../pages/homePage"))
+const Movies = lazy(() => import("../pages/moviesPage"))
+const MovieDetails = lazy(() => import("../pages/movieDetailsPage"))
 const Cast = lazy(() => import("components/cast/cast"))
 const Reviews = lazy(() => import("components/reviews/reviews"))
 const NotFound = lazy(() => import("pages/notFoundPage"))
 
-export const App = () => {
-  return <Routes>
+const App = () => {
+  return (
+    <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
@@ -21,10 +22,7 @@ export const App = () => {
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
+  )
 }
 
-// '/' – компонент Home, домашня сторінка зі списком популярних кінофільмів.
-// '/movies' – компонент Movies, сторінка пошуку кінофільмів за ключовим словом.
-// '/movies/:movieId' – компонент MovieDetails, сторінка з детальною інформацією про кінофільм.
-// /movies/:movieId/cast – компонент Cast, інформація про акторський склад. Рендериться на сторінці MovieDetails.
-// /movies/:movieId/reviews – компонент Reviews, інформація про огляди. Рендериться на сторінці MovieDetails.
+export default App
