6cd2d8ec0ed1b49f2d67a39c15fdd0686be79811
Daria Shpak <darya.shpak@gmail.com>
2023-09-08 18:25:25 +0300

    Update moviesListItem.styled.js

--- src/components/moviesListItem/moviesListItem.styled.js
+++ src/components/moviesListItem/moviesListItem.styled.js
@@ -6,14 +6,24 @@ export const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 14px;
-  width: 207px;
-  height: 347px;
+  width: 212px;
+  height: 370px;
   margin-bottom: 15px;
+  margin-top: 15px;
+padding: 4px;
+  &: focus,
+  &: hover,
+  &: active {
+    background-color: rgba(153, 221, 221, 0.7);
+    border-radius: var(--border-radius);
+  }
 `
 
 export const Img = styled.img`
   display: block;
+  padding: 2px;
   border-radius: var(--border-radius);
+  margin-bottom: 5px;
 `
 
 export const H2 = styled.h2`
