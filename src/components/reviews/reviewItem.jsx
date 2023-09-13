37680acaa67ee300b58451f92efd3e7c6714eaea
Daria Shpak <darya.shpak@gmail.com>
2023-09-13 13:04:13 +0300

    Adds proptypes

--- src/components/reviews/reviewItem.jsx
+++ src/components/reviews/reviewItem.jsx
@@ -1,18 +1,22 @@
 import React from "react"
 import {ReviewsListItem, Author, Content} from "./reviews.styled"
+import PropTypes from "prop-types"
 
-const ReviewItem = ({ author, content }) => {
-
+const ReviewItem = ({author, content}) => {
   return (
     <ReviewsListItem>
       <Author>
-      {author}
+        {author}
       </Author>
       <Content>
         "{content}"
       </Content>
     </ReviewsListItem>
   )
+}
+ReviewItem.propTypes = {
+  author: PropTypes.string.isRequired,
+  contenet: PropTypes.string.isRequired
 }
 
 export default ReviewItem
